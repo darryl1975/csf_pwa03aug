1883ece89c410365ad473b599605257af3f724d8
Darryl <darryl1975@hotmail.com>
2023-08-03 15:05:56 +0800

    pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'csf-pwa03aug';
+
+  isOnline: boolean;
+
+  constructor() {
+    this.isOnline = false;
+  }
+
+  ngOnInit(): void {
+    this.updateOnlineStatus();
+    
+    window.addEventListener('online', this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+  }
+
+  private updateOnlineStatus() : void {
+    this.isOnline = window.navigator.onLine;
+  }
 }
